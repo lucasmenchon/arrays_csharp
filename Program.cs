94280e76dde0952d6bba5fb4c6c0db1f5e2067eb
lucasmenchon <it.lucas@outlook.com>
2022-05-21 00:57:03 -0300

    1 commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+using Arrays.Helper;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[3] { 9, 1, 2 };
+
+            WriteLine("array original");
+            op.ImprimirArray(array);
+
+            op.OrdenarBubbleSort(ref array);
+
+            WriteLine("array ordenado");
+            op.ImprimirArray(array);
+
+
+            WriteLine("");
+            /*int[] arrayInteiros = new int[3];
+            arrayInteiros[0] = 1;
+            arrayInteiros[1] = 2;
+            arrayInteiros[2] = int.Parse("3"); // reconhece apenas numero na string;
+
+            //matriz "multidimensional";
+            int[,] matriz = new int[4, 2] // 4 linhas e 2 colunas.
+            {
+                { 0 , 1 },
+                { 2 , 3 },
+                { 4 , 5 },
+                { 6 , 7 }
+            };
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Write(matriz[i, j]);
+                }
+            }
+
+            //passando valores matriz
+            matriz[0, 0] = 1;
+
+            //for
+            Write("Array pelo For");
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                Write(arrayInteiros[i]);
+            }
+
+            //foreach
+            Write("Array pelo ForEach");
+            foreach (int it in arrayInteiros)
+            {
+                Write(it);
+            }*/
+        }
+    }
+}
