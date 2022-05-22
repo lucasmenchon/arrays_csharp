bb51f4d1eca47978214c3def9f7b5c312353a066
lucasmenchon <it.lucas@outlook.com>
2022-05-22 18:27:35 -0300

    remocao comentarios

--- Program.cs
+++ Program.cs
@@ -49,124 +49,124 @@ namespace Arrays
             //order indice lista
             estados.Insert(1, "RS");
 
-            //OperacoesArray op = new OperacoesArray();
-            //int[] array = new int[3] { 3, 2, 1 };
-            //int[] arrayCopia = new int[5];
-            ////int valorProcurado = 9;
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[3] { 3, 2, 1 };
+            int[] arrayCopia = new int[5];
+            //int valorProcurado = 9;
 
-            ////convertendo array para string
-            //string[] arrayString = op.ConvertArrayString(array);
-
-
-            ////Redimensionando array
-            //Write($"Capacidade atual array: {array.Length}");
-            //op.RedArray(ref array, array.Length * 2);
-            //Write($"Capacidade atual array: {array.Length}");
-
-            ////obter indice
-            //int indice = op.ObterIndic(array, valorProcurado);
-
-            //if (indice > -1)
-            //{
-            //    Write("o indice do elemento {0} e: {1}", valorProcurado, indice);
-            //}
-            //else
-            //{
-            //    Write("nada");
-            //}
-
-            ////encontrar valor
-            //int obterValor = op.ObterValor(array, valorProcurado);
-
-            //if (obterValor > 0)
-            //{
-            //    Write("ok");
-            //}
-            //else
-            //{
-            //    Write("nada");
-            //}
-
-            ////Maior que todos array            
-            //bool tdsMaiorQue = op.TdMaiorQue(array, valorProcurado);
-            //if (tdsMaiorQue)
-            //{
-            //    Write($"Todos são maior que: {valorProcurado}");
-            //}
-            //else
-            //{
-            //    WriteLine("nada");
-            //}
-
-            ////Procurando Array
-            //int valorProcurado = 2;
-            //bool exist = op.Exist(array, valorProcurado);
-
-            //if (exist)
-            //{
-            //    Write($"ok {valorProcurado}");
-            //}
-            //else
-            //{
-            //    Write("nada encontrado");
-            //}
-
-            ////Array Ordenados
-            //Write("array original");
-            //op.ImprimirArray(array);
-
-            //op.OrdenarBubbleSort(ref array);
-            //op.Ordenar(ref array);
+            //convertendo array para string
+            string[] arrayString = op.ConvertArrayString(array);
 
 
-            //Write("array ordenado");
-            //op.ImprimirArray(array);
+            //Redimensionando array
+            Write($"Capacidade atual array: {array.Length}");
+            op.RedArray(ref array, array.Length * 2);
+            Write($"Capacidade atual array: {array.Length}");
 
-            //Write("array antes copia");
-            //op.ImprimirArray(arrayCopia);
+            //obter indice
+            int indice = op.ObterIndic(array, valorProcurado);
 
-            //Write("array pos copia");
-            //op.Copiar(ref array, ref arrayCopia);
-            //op.ImprimirArray(arrayCopia);
+            if (indice > -1)
+            {
+                Write("o indice do elemento {0} e: {1}", valorProcurado, indice);
+            }
+            else
+            {
+                Write("nada");
+            }
 
-            //int[] arrayInteiros = new int[3];
-            //arrayInteiros[0] = 1;
-            //arrayInteiros[1] = 2;
-            //arrayInteiros[2] = int.Parse("3"); // reconhece apenas numero na string;
+            //encontrar valor
+            int obterValor = op.ObterValor(array, valorProcurado);
 
-            ////matriz "multidimensional";
-            //int[,] matriz = new int[4, 2] // 4 linhas e 2 colunas.
-            //{
-            //    { 0 , 1 },
-            //    { 2 , 3 },
-            //    { 4 , 5 },
-            //    { 6 , 7 }
-            //};
+            if (obterValor > 0)
+            {
+                Write("ok");
+            }
+            else
+            {
+                Write("nada");
+            }
 
-            //for (int i = 0; i < matriz.GetLength(0); i++)
-            //{
-            //    for (int j = 0; j < matriz.GetLength(1); j++)
-            //    {
-            //        Write(matriz[i, j]);
-            //    }
-            //}
+            //Maior que todos array            
+            bool tdsMaiorQue = op.TdMaiorQue(array, valorProcurado);
+            if (tdsMaiorQue)
+            {
+                Write($"Todos são maior que: {valorProcurado}");
+            }
+            else
+            {
+                WriteLine("nada");
+            }
 
-            ////passando valores matriz
-            //matriz[0, 0] = 1;
+            //Procurando Array
+            int valorProcurado = 2;
+            bool exist = op.Exist(array, valorProcurado);
 
-            ////for
-            //Write("Array pelo For");
-            //for (int i = 0; i < arrayInteiros.Length; i++)
-            //{
-            //    Write(arrayInteiros[i]);
-            //}
+            if (exist)
+            {
+                Write($"ok {valorProcurado}");
+            }
+            else
+            {
+                Write("nada encontrado");
+            }
 
-            ////foreach
-            //Write("Array pelo ForEach");
-            //foreach (int it in arrayInteiros)
-            //{
-            //    Write(it);
-            //}
+            //Array Ordenados
+            Write("array original");
+            op.ImprimirArray(array);
+
+            op.OrdenarBubbleSort(ref array);
+            op.Ordenar(ref array);
+
+
+            Write("array ordenado");
+            op.ImprimirArray(array);
+
+            Write("array antes copia");
+            op.ImprimirArray(arrayCopia);
+
+            Write("array pos copia");
+            op.Copiar(ref array, ref arrayCopia);
+            op.ImprimirArray(arrayCopia);
+
+            int[] arrayInteiros = new int[3];
+            arrayInteiros[0] = 1;
+            arrayInteiros[1] = 2;
+            arrayInteiros[2] = int.Parse("3"); // reconhece apenas numero na string;
+
+            //matriz "multidimensional";
+            int[,] matriz = new int[4, 2] // 4 linhas e 2 colunas.
+            {
+                { 0 , 1 },
+                { 2 , 3 },
+                { 4 , 5 },
+                { 6 , 7 }
+            };
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Write(matriz[i, j]);
+                }
+            }
+
+            //passando valores matriz
+            matriz[0, 0] = 1;
+
+            //for
+            Write("Array pelo For");
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                Write(arrayInteiros[i]);
+            }
+
+            //foreach
+            Write("Array pelo ForEach");
+            foreach (int it in arrayInteiros)
+            {
+                Write(it);
+            }
         }
     }
 }
